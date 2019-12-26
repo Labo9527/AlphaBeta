c5e803886e1773f669425f96bfcd6aed9dd57a5c
yzt <2543761065@qq.com>
2019-12-26 23:21:10 +0800

    improve tree drawing and add leaf node function

--- asset/my.js
+++ asset/my.js
@@ -9,19 +9,42 @@ window.onload = function(){
     bigcanvas.width = 700;
     var ctx = bigcanvas.getContext("2d");
     ctx.beginPath();
-    ctx.arc(350,50,10,0,2*Math.PI);
+    ctx.arc(350,50,5,0,2*Math.PI);
     ctx.fillStyle="blue";
     ctx.fill();
     bigcanvas.addEventListener("mousedown", onDown);
 
-    root.x = 350;
+    root.x = 250;
     root.y = 50;
     root.type = "max";
     nodes[0] = root;
 
     $("button#test1").click(function(){
-        
         // ReDraw(root, 0);
+        for(let i=0;i<nodes.length;i++){
+            if(nodes[i].picked==0){
+                if(i==nodes.length-1){
+                    return;
+                }
+                continue;
+            }
+            var theResponse = window.prompt("请输入叶子节点的值");
+            console.log(theResponse);
+            nodes[i].children.push(new node());
+            nodes[i].children[nodes[i].children.length-1].type="leaf";
+            nodes[i].children[nodes[i].children.length-1].value=theResponse;
+            nodes[i].children[nodes[i].children.length-1].par=nodes[i];
+            nodes.push(nodes[i].children[nodes[i].children.length-1]);
+            nodes[i].children[nodes[i].children.length-1].y=nodes[i].y + 50;
+            nodes[i].children[nodes[i].children.length-1].x=nodes[i].x;
+            break;
+        }
+        getDepth(root, 0);
+        ReDraw(root, 0);
+        for(let i=0;i<nodes.length;i++){
+            nodes[i].picked=0;
+        }
+        console.log(tower);
     });
 
     $("button#test2").click(function(){
@@ -31,6 +54,9 @@ window.onload = function(){
                     return;
                 }
                 continue;
+            }
+            if(nodes[i].type=="leaf"){
+                return;
             }
             nodes[i].children.push(new node());
             nodes[i].children[nodes[i].children.length-1].par=nodes[i];
@@ -68,12 +94,12 @@ function onDown(e){
     getDepth(root ,0);
     ReDraw(root, 0);
     for(let i=0;i<nodes.length;i++){
-        if(abs(mx-nodes[i].x)<10&&abs(my-nodes[i].y)<10){
+        if(abs(mx-nodes[i].x)<10&&abs(my-nodes[i].y)<10&&nodes[i].type!="leaf"){
             nodes[i].picked=1;
             var ctx = bigcanvas.getContext("2d");
             ctx.beginPath();
             ctx.lineWidth=3;
-            ctx.arc(nodes[i].x, nodes[i].y , 10,0,2*Math.PI);
+            ctx.arc(nodes[i].x, nodes[i].y , 5,0,2*Math.PI);
             ctx.strokeStyle="green";
             ctx.stroke();
         }
@@ -136,19 +162,33 @@ function ReDraw(node, cnt){
         }
     }
     if(node.par==0){
-        node.y = 50 + cnt*50;
-        node.x = 350 + 50 * (getIndex(tower[cnt],node)-Math.floor(tower[cnt].length/2));
+        node.y = 50 + cnt*25;
+        node.x = 250 + 25 * (getIndex(tower[cnt],node)-Math.floor(tower[cnt].length/2));
     } else{
-        node.y = 50 + cnt*50;
-        node.x = node.par.x + 50 * (getIndex(node.par.children,node)-Math.floor(node.par.children.length/2));
+        console.log("my",cnt);
+        node.y = 50 + cnt*25;
+        node.x = node.par.x + 25 * (1) * (getIndex(node.par.children,node)-Math.floor(node.par.children.length/2));
         while(GetCrast(node.x,node.y)!=1){
-            node.x += 50;
+            node.x += 25 * 1;
         }
     }
     for(let i=0;i<node.children.length;i++){
         ReDraw(node.children[i], cnt+1);
     }
     if(cnt==0){
+
+
+        for(let i=tower.length-1;i>=0;i--){
+            for(let j=tower[i].length-1;j>=0;j--){
+                if(tower[i][j].children.length==0)
+                    continue;
+                while(tower[i][j].children[tower[i][j].children.length-1].x-100>tower[i][j].x){
+                    tower[i][j].x+=100;
+                }
+            }
+        }
+
+
         var width = document.getElementById("bigcanvas").width;
         var height = document.getElementById("bigcanvas").height;
         var cxt=document.getElementById("bigcanvas").getContext("2d");
@@ -156,14 +196,17 @@ function ReDraw(node, cnt){
         for(let i=0;i<nodes.length;i++){
             var ctx = bigcanvas.getContext("2d");
             ctx.beginPath();
-            ctx.arc(nodes[i].x,nodes[i].y,10,0,2*Math.PI);
+            ctx.arc(nodes[i].x,nodes[i].y,5,0,2*Math.PI);
             if(nodes[i].type=="max")
                 ctx.fillStyle="blue";
             else if(nodes[i].type=="min")
                 ctx.fillStyle="red";
+            else if(nodes[i].type=="leaf")
+                ctx.fillStyle="green";
             ctx.fill();
             ctx.strokeStyle = "black";
             for(let j=0;j<nodes[i].children.length;j++){
+                ctx.lineWidth = 2;
                 ctx.moveTo(nodes[i].x, nodes[i].y);
                 ctx.lineTo(nodes[i].children[j].x, nodes[i].children[j].y);
                 ctx.stroke();
@@ -180,4 +223,5 @@ function node(){
     this.children=new Array();
     this.par= 0;
     this.picked=0;
+    this.value=0;
 }
